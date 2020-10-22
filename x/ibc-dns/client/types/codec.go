324d2340d0e32868d7872bd4b3e90190b2020d83
toshihiko-okubo <toshihiko.okubo@speee.jp>
2020-10-22 17:10:51 +0900

    fix dnsclient's codec registration

--- x/ibc-dns/client/types/codec.go
+++ x/ibc-dns/client/types/codec.go
@@ -17,6 +17,6 @@ func init() {
 
 // RegisterCodec registers concrete types on the Amino codec
 func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgRegisterDomain{}, "ibc/dns/client/MsgRegisterDomain", nil)
-	cdc.RegisterConcrete(MsgDomainAssociationCreate{}, "ibc/dns/client/MsgDomainAssociationCreate", nil)
+	cdc.RegisterConcrete(&MsgRegisterDomain{}, "ibc/dns/client/MsgRegisterDomain", nil)
+	cdc.RegisterConcrete(&MsgDomainAssociationCreate{}, "ibc/dns/client/MsgDomainAssociationCreate", nil)
 }
